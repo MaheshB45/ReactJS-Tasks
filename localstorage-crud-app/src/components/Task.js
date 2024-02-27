812c24beaa80627e1832fce4d8b66a2232398265
Mahesh Bahir <maheshbahir45@gmail.com>
2024-02-27 14:18:38 +0530

    fixed and update

--- localstorage-crud-app/src/components/Task.js
+++ localstorage-crud-app/src/components/Task.js
@@ -26,10 +26,10 @@ const Task = ({ id, title, completed, onCompleteTask, onEditTask, onDeleteTask }
             <>
               <div className='lg:grow h-8 flex items-center lg:gap-x-2 overflow-hidden'>
                 <span className={`${completedClass} lg:order-2 w-full pl-1 text-white dark:text-gray-200 rounded overflow-hidden overflow-ellipsis`}>{title}</span>
-                <button onClick={onComplete} className='lg:order-1 h-full px-1 lg:px-3 bg-green-500 hover:bg-green-300 rounded transition-colors'>Done</button>
+                <button onClick={onComplete} className='lg:order-1 h-full px-1 lg:px-3 hover:text-white bg-green-500 hover:bg-green-600 rounded transition-colors'>Done</button>
               </div>
               <div className='flex lg:gap-x-2'>
-                <button onClick={onEdit} className='grow basis-1 lg:px-3 bg-yellow-500 hover:bg-yellow-300 rounded transition-colors'>Edit</button>
+                <button onClick={onEdit} className='grow basis-1 lg:px-3 hover:text-white bg-yellow-500 hover:bg-yellow-600 rounded transition-colors'>Edit</button>
                 <button onClick={onDelete} className='grow basis-0 lg:px-3 hover:text-white dark:hover:text-gray-200 bg-red-500 hover:bg-red-600 rounded transition-colors'>Delete</button>
               </div>
             </>
